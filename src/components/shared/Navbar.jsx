f40007197f4640a29e260f57822ba7a319da3a36
016Aditya <016adityasingh@gmail.com>
2025-04-22 21:57:33 +0530

    Login page linked with navbar button

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,50 +1,54 @@
-
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import { Link } from "react-router-dom"
-
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
     <div className="flex flex-row mx-14 my-8">
-      <div className="flex-1"><Link to="/"><img src="https://zerodha.com/varsity/wp-content/themes/varsity2/images/logo.png" alt="" /></Link></div>
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
+      <div className="flex-1">
+        <Link to="/">
+          <img
+            src="https://zerodha.com/varsity/wp-content/themes/varsity2/images/logo.png"
+            alt=""
+          />
+        </Link>
+      </div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Modules
             </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Contact
             </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Videos
             </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Certified
-            </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/login" className={navigationMenuTriggerStyle()}>
+              Login
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Blog
             </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-      
-    </NavigationMenu>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
